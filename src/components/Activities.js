0a5afb89f566d145d9501cb070ba85091a85896c
Parul thakur <paru.thakur11oct@gmail.com>
2022-11-24 17:27:06 +0530

    CSS added

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import './Activites.css'
 import image from '../assets/children.jpeg'
-import rect from '../assets/Rectangle 15.png'
 const Activities = () => {
   return (
     <>
         <div className="activity">
-            <div className="text">
-             <h1>ACTIVITIES</h1>
-             <img src={rect} alt="" />
-            </div>
-            <div className="gallery">
-                <img src={image} alt="" />
-            </div>
+
         </div>
     </>
   )
---
+++ src/components/Images.js
@@ -0,0 +1,7 @@
+import image1 from '../assets/children1jpg.jpg';
+import image2 from '../assets/children27.jpg'
+import image3 from '../assets/children3.jpg'
+import image4 from '../assets/children4.jpg'
+import image5 from '../assets/children5.jpg'
+
+export default [image1, image2, image3, image4, image5];
