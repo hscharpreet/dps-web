f7aef588b396d800c09360948e30b82238c30b80
Parul thakur <paru.thakur11oct@gmail.com>
2023-03-24 22:16:35 +0530

    added slider

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import Navbar from "./Navbar";
+import Navbar from "./Navbar";
 import "./Header.css";
 import Slider from "./slider/Slider";
 
@@ -7,12 +7,13 @@ const Header = () => {
   return (
     <>
       <div className="header">
+{/* <Navbar/> */}
         <h1 className="animate-charcter">
           WELCOME TO <br />
           <span> DIVINE</span> <br /> PUBLIC SCHOOL
         </h1>
 
-        {/* <Slider /> */}
+        <Slider />
       </div>
     </>
   );
