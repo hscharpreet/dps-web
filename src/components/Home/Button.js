5d5870aeaa0a26708abf7e07a48deca2255d050c
Parul thakur <paru.thakur11oct@gmail.com>
2023-03-26 01:38:16 +0530

    made some changes in css

--- src/components/Home/Button.js
+++ src/components/Home/Button.js
@@ -23,20 +23,25 @@ const Button = () => {
         </div>
         <nav className={sidebar ? "navMenu active" : "navMenu"}>
           <RxCross1 onClick={showSidebar} className="menuBars2"></RxCross1>
-
-          <ul className="navMenuItem" onClick={showSidebar}>
-            <li className="navbarToggle"></li>
-            {SidebarData.map((item, index) => {
-              return (
-                <li key={index} className={item.className}>
-                  <NavLink to={item.path} >
-                   {item.title}
-                  </NavLink>
-                  <hr className="line5" />
-                </li>
-              );
-            })}
-          </ul>
+          <div className="menuImage">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/divine-public-school-e196f.appspot.com/o/websiteImages%2Fhome%2Fchildrenback.webp?alt=media&token=84e7b013-1c16-4b9b-b80b-ab534e35124a"
+              alt=""
+            />
+          </div>
+          <div className="navMenuItem">
+            <ul onClick={showSidebar}>
+              <li className="navbarToggle"></li>
+              {SidebarData.map((item, index) => {
+                return (
+                  <li key={index} className={item.className}>
+                    <NavLink to={item.path}>{item.title}</NavLink>
+                    {/* <hr className="line5" /> */}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </nav>
       </IconContext.Provider>
     </div>
